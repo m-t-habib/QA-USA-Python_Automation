1dff42444e1b882ae61a7af628c02d574c07a728
m-t-habib <mdtareqhabib77@gmail.com>
2025-04-16 16:27:41 -0400

    First Update

--- data.py
+++ data.py
@@ -1,1 +1,12 @@
+# URL for the Urban Routes server
+
+URBAN_ROUTES_URL = 'https://cnt-1eb6b92b-a285-4565-bdbc-8ce02d1049c3.containerhub.tripleten-services.com/'
+
 # File with constants
+
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
